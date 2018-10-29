0faae504c2349884338920c67ee2f06e3015bfa5
AkashSReddy <akashsreddy98@gmail.com>
2018-10-30 00:54:58 +0530

    hashing added at login and register, needs to be tested with database

--- inventorymain.js
+++ inventorymain.js
@@ -8,64 +8,69 @@ const port = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
-const login=require("./model/login.js")
+const login = require("./model/login.js");
 const app = express();
 const products = require("./model/products.js");
+var crypto = require("crypto");
 const definitions = require("./definitions.js");
-var meraname ='Adidas';
-var mName ='Adidas';
+var meraname = "Adidas";
+var mName = "Adidas";
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + "/views/partials");
 mongoose.connect(
   "mongodb://localhost/inventory",
   { useNewUrlParser: "true" }
 );
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 hbs.registerHelper("screamIt", text => {
   return text.toUpperCase();
 });
 
-app.post("/",function(req,res)
-{
-  var check={
-    email:req.body.email,
-    password:req.body.password
-          }
-  login.login.findOne({email:req.body.email,password:req.body.password}, function(err, docs) {
-        console.log(docs);
-        //console.log(docs.company);
-       if(docs)
-       {
-         if(docs.role==="Distributor")
-         {
-           products.products.find({name:docs.company}, {}, function(err, docs2) {
-             meraname=docs.company;
-             console.log(docs2);
-             res.render("home.hbs", {
-               pageTitle: meraname,
-               productlist: docs2
-             });
-           });
-         }
-         else if(docs.role==="Customer")
-         {
-           products.products.find({}, {}, function(err, docs) { //<% %>
-               console.log(docs);
-               res.render("homecustomer.hbs", {
-               pageTitle: "Inventory",
-               productlist: docs
-             });
-            });
-         }
-       }
-       else {
-        res.render('loginpage.hbs');
-       }
-     });
-}
-);
+app.post("/", function(req, res) {
+  var newpass = crypto
+    .createHash("sha256")
+    .update(req.body.password, "utf8")
+    .digest("hex");
+  console.log(newpass);
+  var check = {
+    email: req.body.email,
+    password: newpass
+  };
+  login.login.findOne({ email: req.body.email, password: newpass }, function(
+    err,
+    docs
+  ) {
+    console.log(docs);
+    //console.log(docs.company);
+    if (docs) {
+      if (docs.role === "Distributor") {
+        products.products.find({ name: docs.company }, {}, function(
+          err,
+          docs2
+        ) {
+          meraname = docs.company;
+          console.log(docs2);
+          res.render("home.hbs", {
+            pageTitle: meraname,
+            productlist: docs2
+          });
+        });
+      } else if (docs.role === "Customer") {
+        products.products.find({}, {}, function(err, docs) {
+          //<% %>
+          console.log(docs);
+          res.render("homecustomer.hbs", {
+            pageTitle: "Inventory",
+            productlist: docs
+          });
+        });
+      }
+    } else {
+      res.render("loginpage.hbs");
+    }
+  });
+});
 
 app.get("/logout", (req, res) => {
   req.logout();
@@ -80,7 +85,7 @@ app.get("/", (req, res) => {
 
 app.get("/home", (req, res) => {
   products.products.find({}, {}, function(err, docs) {
-    console.log(docs);
+    // console.log(docs);
     res.render("home.hbs", {
       pageTitle: meraname,
       productlist: docs
@@ -89,7 +94,7 @@ app.get("/home", (req, res) => {
 });
 app.get("/homecustomer", (req, res) => {
   products.products.find({}, {}, function(err, docs) {
-    console.log(docs);
+    // console.log(docs);
     res.render("homecustomer.hbs", {
       pageTitle: "Inventory",
       productlist: docs
@@ -97,7 +102,7 @@ app.get("/homecustomer", (req, res) => {
   });
 });
 
-app.get("/buy",(req,res) =>{
+app.get("/buy", (req, res) => {
   res.render("buyquantity.hbs", {
     pageTitle: "What do you want to buy?"
   });
@@ -108,38 +113,37 @@ app.get("/register", (req, res, next) => {
 });
 
 app.post("/register", function(req, res, next) {
-
-login.login.findOne({email:req.body.email}, function(err, docs) {
-      console.log(docs);
-      console.log(req.body.type);
-     if(docs)
-     {
-       res.render('register.hbs');
-     }
-     else {
-       if(req.body.type==="Distributor")
-       {
-       var newuser={
-         name:req.body.username,
-         email:req.body.email,
-         role:req.body.type,
-         password:req.body.password,
-         company:req.body.company
-       };
-     }
-     else{
-       var newuser={
-         name:req.body.username,
-         email:req.body.email,
-         role:req.body.type,
-         password:req.body.password
-       };
-     }
-       var newuser1=new login.login(newuser);
-       newuser1.save()
-      res.render('loginpage.hbs');
-     }
-   });
+  login.login.findOne({ email: req.body.email }, function(err, docs) {
+    console.log(docs);
+    console.log(req.body.type);
+    var newpass = crypto
+      .createHash("sha256")
+      .update(req.body.password, "utf8")
+      .digest("hex");
+    if (docs) {
+      res.render("register.hbs");
+    } else {
+      if (req.body.type === "Distributor") {
+        var newuser = {
+          name: req.body.username,
+          email: req.body.email,
+          role: req.body.type,
+          password: newpass,
+          company: req.body.company
+        };
+      } else {
+        var newuser = {
+          name: req.body.username,
+          email: req.body.email,
+          role: req.body.type,
+          password: newpass
+        };
+      }
+      var newuser1 = new login.login(newuser);
+      newuser1.save();
+      res.render("loginpage.hbs");
+    }
+  });
 });
 app.get("/addcategory", (req, res) => {
   res.render("addcategory.hbs", {
@@ -173,7 +177,7 @@ app.post("/addcategory", function(req, res) {
     pageTitle: message
   });
 });
-app.post("/addq",  function(req, res) {
+app.post("/addq", function(req, res) {
   var bool = definitions.Addquantity(
     meraname,
     req.body.product,
@@ -186,7 +190,7 @@ app.post("/addq",  function(req, res) {
   });
 });
 
-app.post("/removeq",  function(req, res) {
+app.post("/removeq", function(req, res) {
   var bool = definitions.Removequantity(
     meraname,
     req.body.product,
@@ -199,7 +203,7 @@ app.post("/removeq",  function(req, res) {
   });
 });
 
-app.post("/buy",  function(req, res) {
+app.post("/buy", function(req, res) {
   var bool = definitions.Buy(
     meraname,
     req.body.product,
@@ -212,7 +216,6 @@ app.post("/buy",  function(req, res) {
   });
 });
 
-
 app.get("/list", (req, res) => {
   fs.readFile(__dirname + "/" + "categories.json", "utf8", (err, data) => {
     res.end(data);
@@ -224,7 +227,7 @@ app.get("/data", (req, res) => {
   //fs.readFile(__dirname + "/" + "categories.json", 'utf8', (err, data) => {
   //console.log("Data: Name " + req.body.name);
   console.log("Name: " + meraname);
-  products.products.find({"name":meraname}, {}, function(err, docs) {
+  products.products.find({ name: meraname }, {}, function(err, docs) {
     console.log(docs);
     res.send(docs);
   });
@@ -236,8 +239,7 @@ app.get("/dataquantity", (req, res) => {
   products.products.find({}, {}, function(err, docs) {
     console.log(docs);
     res.send(docs);
-  }
-);
+  });
 });
 
 app.get("/view", (req, res) => {
